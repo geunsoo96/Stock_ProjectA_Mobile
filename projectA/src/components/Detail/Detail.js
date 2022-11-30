eab9eccbb1bf83374f6b71a991233a70cb122b8e
jongin12 <jongin981224@gmail.com>
2022-11-30 17:25:14 +0900

    Detail 하위컴포넌트 제작(그래프 제외)

--- projectA/src/components/Detail/Detail.js
+++ projectA/src/components/Detail/Detail.js
@@ -1,21 +1,67 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import DetailName from './DetailName';
+import DetailPrice from './DetailPrice';
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'red',
-    fontSize: 50,
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  nameBox: {
+    height: 50,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+  },
+  graphBox: {
+    height: 300,
+  },
+  buttonBox: {
+    height: 60,
+    backgroundColor: 'blue',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    padding: 10,
+  },
+  priceBox: {
+    height: 420,
+    backgroundColor: 'pink',
   },
 });
 
 export default function Detail(props) {
+  const nameDummyData = {
+    name: '동화약품',
+    code: '000020',
+  };
+  const priceDummyData = {
+    open: 10000,
+    high: 12000,
+    low: 9000,
+    close: 11000,
+    volume: 111111,
+    day: '2022-02-20',
+  };
+  const [nameData, nameDataSet] = useState(nameDummyData);
+  const [graphData, graphDataSet] = useState();
+  const [priceData, priceDataSet] = useState(priceDummyData);
+  const onPress = () => {};
   return (
-    <View>
-      <Text style={styles.text}>너무힘들다 {props.name}</Text>
+    <View style={styles.container}>
+      <View style={styles.nameBox}>
+        <DetailName data={nameData} />
+      </View>
+      <View style={styles.graphBox}>
+        <Text>그래프 컴포넌트</Text>
+      </View>
+      <View style={styles.buttonBox}>
+        <Button title="1주일" onPress={onPress} />
+        <Button title="1개월" onPress={onPress} />
+        <Button title="1년" onPress={onPress} />
+      </View>
+      <View style={styles.priceBox}>
+        <DetailPrice data={priceData} />
+      </View>
     </View>
   );
 }
-
-Detail.defaultProps = {
-  name: 'ㄹㅇ',
-};
---
+++ projectA/src/components/Detail/DetailPrice.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+  },
+  priceBox: {
+    width: '50%',
+    flexDirection: 'row',
+  },
+  text: {
+    width: '100%',
+    fontSize: 40,
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
+
+export default function DetailPrice(props) {
+  const dataSet = [
+    {kr: '시가', en: 'open'},
+    {kr: '고가', en: 'high'},
+    {kr: '저가', en: 'low'},
+    {kr: '종가', en: 'close'},
+    {kr: '거래량', en: 'volume'},
+  ];
+  return (
+    <View style={styles.container}>
+      {dataSet.map(item => {
+        return (
+          <View style={styles.priceBox}>
+            <Text style={styles.text}>{item.kr}</Text>
+            <Text style={styles.text}>{props.data[item.en]}</Text>
+          </View>
+        );
+      })}
+    </View>
+  );
+}
