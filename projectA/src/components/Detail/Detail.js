a0fa806e27759c3e7aa5854e95cecfc486e85581
jongin12 <jongin981224@gmail.com>
2022-11-30 14:35:32 +0900

    Detail 추가

--- projectA/src/components/Detail/Detail.js
+++ projectA/src/components/Detail/Detail.js
@@ -1,1 +1,21 @@
-console.log('종인');
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'red',
+    fontSize: 50,
+  },
+});
+
+export default function Detail(props) {
+  return (
+    <View>
+      <Text style={styles.text}>너무힘들다 {props.name}</Text>
+    </View>
+  );
+}
+
+Detail.defaultProps = {
+  name: 'ㄹㅇ',
+};
