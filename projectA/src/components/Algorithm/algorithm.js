a5a1b025d36d11a74469aaf7cb3b66ac4555d572
KinGeunSoo <geunsoo1106@gmail.com>
2022-11-30 17:26:34 +0900

    알고리즘 페이지 마크업 진행중

--- projectA/src/components/Algorithm/algorithm.js
+++ projectA/src/components/Algorithm/algorithm.js
@@ -1,34 +1,65 @@
-import {StyleSheet, Text, View, Image} from 'react-native';
+import {StyleSheet, Text, View, Image, Button, Pressable} from 'react-native';
 import React from 'react';
 import {ScrollView} from 'react-native';
+
 const Algorithm = () => {
+  // const dummyData
   return (
     <ScrollView
       style={{
-        height: 100 + '%',
+        height: '100%',
       }}>
-      <View style={styles.container}>
-        <Text style={styles.contentBox}>kospi</Text>
-        <Text style={styles.contentBox}>kosdaq</Text>
+      <View style={styles.head}>
+        <Text style={styles.headText}>이달의 추천 주식</Text>
+        <Button title="침팬지 추천받기" color="salmon" />
+        <Pressable style={styles.button} title="hi"></Pressable>
       </View>
+      <View style={styles.container}>
+        <Text style={styles.contentBox}>KOSPI</Text>
+        <Text style={styles.contentBox}>KOSDAQ</Text>
+      </View>
+      <View></View>
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     display: 'flex',
     // flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: 'orange',
-    width: 100 + '%',
-    height: 100 + '%',
+    width: '100%',
+    height: '100%',
   },
   contentBox: {
-    backgroundColor: 'red',
-    width: 80 + '%',
-    height: 100 + '%',
+    // backgroundColor: '#FFC33B',
+    borderStyle: 'solid',
+    borderColor: '#FFC33B',
+    borderWidth: 5,
+    borderRadius: 20,
+    width: 300,
+    height: 600,
+    margin: 5,
+    padding: 20,
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  head: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  headText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  button: {
+    width: '60%',
+    height: '10%',
+    backgroundColor: '#FFC33B',
   },
 });
 export default Algorithm;
