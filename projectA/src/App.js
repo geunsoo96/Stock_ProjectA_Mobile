d04a371a6176a29937c703539d4228589a45f999
jeonhyoungmin <jhmin7509@gmail.com>
2022-12-01 16:41:32 +0900

    하단 탭 네비게이션

--- projectA/src/App.js
+++ projectA/src/App.js
@@ -2,26 +2,29 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Main from './Layout/Main';
 import Algorithm from './components/Algorithm/algorithm';
 import News from './components/News/News.js';
 import Detail from './components/Detail/Detail';
+import Search from './components/Search/Search';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
-        <Stack.Navigator
+        <Tab.Navigator
           initialRouteName="Main"
           screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Main" component={Main} />
-          <Stack.Screen name="News" component={News} />
-          <Stack.Screen name="Detail" component={Detail} />
-          <Stack.Screen name="Algorithm" component={Algorithm} />
-        </Stack.Navigator>
+          <Tab.Screen name="Search" component={Detail} />
+          <Tab.Screen name="Main" component={Main} />
+          <Tab.Screen name="Algorithm" component={Algorithm} />
+          <Tab.Screen name="News" component={News} />
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
--- projectA/src/components/News/News.js
+++ projectA/src/components/News/News.js
@@ -1,9 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
+import Header from '../../Layout/Header';
+
 const News = () => {
   return (
     <View>
+      <Header />
       <Text>News</Text>
     </View>
   );
