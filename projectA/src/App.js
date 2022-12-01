2ddf3f48c6f4c8aa6ed800fcea79484d4b85950e
jeonhyoungmin <jhmin7509@gmail.com>
2022-12-01 16:58:25 +0900

    하단 탭 작성

--- projectA/src/App.js
+++ projectA/src/App.js
@@ -1,14 +1,15 @@
 import {SafeAreaView, StyleSheet} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon2 from 'react-native-vector-icons/Entypo';
 
 import Main from './Layout/Main';
 import Algorithm from './components/Algorithm/algorithm';
 import News from './components/News/News.js';
 import Detail from './components/Detail/Detail';
-import Search from './components/Search/Search';
 
 // const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -20,10 +21,46 @@ export default function App() {
         <Tab.Navigator
           initialRouteName="Main"
           screenOptions={{headerShown: false}}>
-          <Tab.Screen name="Search" component={Detail} />
-          <Tab.Screen name="Main" component={Main} />
-          <Tab.Screen name="Algorithm" component={Algorithm} />
-          <Tab.Screen name="News" component={News} />
+          <Tab.Screen
+            name="Search"
+            component={Detail}
+            options={{
+              title: '검색',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="search" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Main"
+            component={Main}
+            options={{
+              title: '메인',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Algorithm"
+            component={Algorithm}
+            options={{
+              title: '추천',
+              tabBarIcon: ({color, size}) => (
+                <Icon name="recommend" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="News"
+            component={News}
+            options={{
+              title: '뉴스',
+              tabBarIcon: ({color, size}) => (
+                <Icon2 name="news" color={color} size={size} />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaView>
--- projectA/src/Layout/Main.js
+++ projectA/src/Layout/Main.js
@@ -8,25 +8,25 @@ export default function Main() {
   const navigation = useNavigation();
 
   return (
-    <ScrollView style={styles.container}>
+    <>
       <Header />
-      <Pressable onPress={() => navigation.navigate('News')}>
-        <Text>뉴스</Text>
-      </Pressable>
-      <Pressable onPress={() => navigation.navigate('Detail')}>
-        <Text>디테일</Text>
-      </Pressable>
-      <Pressable onPress={() => navigation.navigate('Algorithm')}>
-        <Text>알고리즘</Text>
-      </Pressable>
-    </ScrollView>
+      <ScrollView style={styles.container}>
+        <Pressable onPress={() => navigation.navigate('News')}>
+          <Text>뉴스</Text>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate('Detail')}>
+          <Text>디테일</Text>
+        </Pressable>
+        <Pressable onPress={() => navigation.navigate('Algorithm')}>
+          <Text>알고리즘</Text>
+        </Pressable>
+      </ScrollView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: 'skyblue',
-    height: 100,
   },
 });
