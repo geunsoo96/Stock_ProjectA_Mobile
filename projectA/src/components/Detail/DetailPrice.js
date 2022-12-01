32162ecfd87ebea3c7042d5a9c24af766e746c91
jongin12 <jongin981224@gmail.com>
2022-12-01 16:32:07 +0900

    map에 key 추가

--- projectA/src/components/Detail/DetailPrice.js
+++ projectA/src/components/Detail/DetailPrice.js
@@ -30,7 +30,7 @@ export default function DetailPrice(props) {
     <View style={styles.container}>
       {dataSet.map(item => {
         return (
-          <View style={styles.priceBox}>
+          <View style={styles.priceBox} key={item.kr}>
             <Text style={styles.text}>{item.kr}</Text>
             <Text style={styles.text}>{props.data[item.en]}</Text>
           </View>
