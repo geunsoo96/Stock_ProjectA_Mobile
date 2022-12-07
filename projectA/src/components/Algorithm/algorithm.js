d5c23345746b512fb6d6846c93795df208376388
KinGeunSoo <geunsoo1106@gmail.com>
2022-12-07 11:04:23 +0900

    알고리즘 추천주 처음부터 다시 작업

--- projectA/src/components/Algorithm/algorithm.js
+++ projectA/src/components/Algorithm/algorithm.js
@@ -1,3 +1,133 @@
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   Image,
+//   Button,
+//   Pressable,
+//   Touchable,
+//   TouchableOpacity,
+// } from 'react-native';
+// import {useState} from 'react';
+// import React from 'react';
+// import {ScrollView} from 'react-native';
+// import Header from '../../Layout/Header';
+
+// const Algorithm = () => {
+//   const [kospi, setKospi] = React.useState('');
+//   const dummyKospi = [
+//     '삼성전자',
+//     'SK하이닉스',
+//     'DB하이텍',
+//     '네이버',
+//     '카카오',
+//     '현대차',
+//     '기아차',
+//     '현대중공업',
+//     '에스원',
+//     '서울식품',
+//   ];
+//   const dummyKosdaq = [
+//     '노란우산',
+//     '한화리조트',
+//     '태양일수',
+//     '대명리조트',
+//     '대전식품',
+//     '에스텍',
+//     '혼다차',
+//     '논산철강',
+//     '청주딸기',
+//     '감자보험',
+//   ];
+//   return (
+//     //   <Tab.Screen
+//     //   name="Algorithm"
+//     //   component={Algorithm}
+//     //   options={{
+//     //     title: '추천',
+//     //     tabBarIcon: ({color, size}) => (
+//     //       <Icon name="recommend" color={color} size={size} />
+//     //     ),
+//     //   }}
+//     // />
+//     <ScrollView
+//       style={{
+//         height: '100%',
+//       }}>
+//       <Header />
+//       <View style={styles.head}>
+//         <Text style={styles.headText}>이달의 추천 주식</Text>
+//         {/* <Button title="침팬지 추천받기" color="salmon" /> */}
+//         <TouchableOpacity style={styles.button}>
+//           <Text style={{color: 'white'}}>침팬지 추천받기</Text>
+//         </TouchableOpacity>
+//       </View>
+//       <View style={styles.container}>
+//         <Text style={styles.contentBox}>
+//           KOSPI
+//           <Text>{dummyKospi}</Text>
+//         </Text>
+//         <Text style={styles.contentBox}>
+//           KOSDAQ
+//           <Text>{dummyKosdaq}</Text>
+//         </Text>
+//       </View>
+//       <View></View>
+//     </ScrollView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'space-around',
+//     alignItems: 'center',
+//     width: '100%',
+//     height: '100%',
+//   },
+//   contentBox: {
+//     // backgroundColor: '#FFC33B',
+//     borderStyle: 'solid',
+//     borderColor: '#FFC33B',
+//     borderWidth: 5,
+//     borderRadius: 20,
+//     width: 300,
+//     height: 600,
+//     margin: 5,
+//     padding: 20,
+//     textAlign: 'center',
+//     fontSize: 20,
+//     display: 'flex',
+//     flexDirection: 'column',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   head: {
+//     width: '100%',
+
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   headText: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//     textAlign: 'center',
+//   },
+//   button: {
+//     width: '30%',
+//     height: '15%',
+//     backgroundColor: '#FFC33B',
+//     borderRadius: 3,
+//     // flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
+// export default Algorithm;
+
 import {
   StyleSheet,
   Text,
@@ -6,6 +136,7 @@ import {
   Button,
   Pressable,
   Touchable,
+  TouchableOpacity,
 } from 'react-native';
 import {useState} from 'react';
 import React from 'react';
@@ -39,94 +170,26 @@ const Algorithm = () => {
     '감자보험',
   ];
   return (
-    //   <Tab.Screen
-    //   name="Algorithm"
-    //   component={Algorithm}
-    //   options={{
-    //     title: '추천',
-    //     tabBarIcon: ({color, size}) => (
-    //       <Icon name="recommend" color={color} size={size} />
-    //     ),
-    //   }}
-    // />
-    <ScrollView
-      style={{
-        height: '100%',
-      }}>
-      <Header />
-      <View style={styles.head}>
-        <Text style={styles.headText}>이달의 추천 주식</Text>
-        {/* <Button title="침팬지 추천받기" color="salmon" /> */}
-        <Tab.Screen
-          name="Algorithm"
-          component={Algorithm}
-          options={{
-            title: '추천',
-            tabBarIcon: ({color, size}) => (
-              <Icon name="recommend" color={color} size={size} />
-            ),
-          }}
-        />
-        <Pressable style={styles.button} title="hi"></Pressable>
+    <ScrollView>
+      <View>
+        <Text>이달의 추천 주식</Text>
+        <TouchableOpacity>
+          <Text>침팬지 추천받기</Text>
+        </TouchableOpacity>
+        <View>
+          {/* kospi */}
+          <View></View>
+          <View></View>
+        </View>
+        <View>
+          {/* kosdaq */}
+          <View></View>
+          <View></View>
+        </View>
       </View>
-      <View style={styles.container}>
-        <Text style={styles.contentBox}>
-          KOSPI
-          <Text>{dummyKospi}</Text>
-        </Text>
-        <Text style={styles.contentBox}>
-          KOSDAQ
-          <Text>{dummyKosdaq}</Text>
-        </Text>
-      </View>
-      <View></View>
     </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    width: '100%',
-    height: '100%',
-  },
-  contentBox: {
-    // backgroundColor: '#FFC33B',
-    borderStyle: 'solid',
-    borderColor: '#FFC33B',
-    borderWidth: 5,
-    borderRadius: 20,
-    width: 300,
-    height: 600,
-    margin: 5,
-    padding: 20,
-    textAlign: 'center',
-    fontSize: 20,
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  head: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  button: {
-    width: '30%',
-    height: '15%',
-    backgroundColor: '#FFC33B',
-    borderRadius: 3,
-  },
-});
+const styles = StyleSheet.create({});
 export default Algorithm;
